9b2565f6aaacdc2ce6a76830c2cb2d928d1acdc5
Bruno <brunocostascuciato009@gmail.com>
2025-04-18 13:28:07 -0300

    2 - Exercicio 1

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// Exercicio 1!
+// Declarando variavel undefined
+let variavelUndefined;
+console.log(variavelUndefined);
+
+// Declarando variavel vazia
+let variavelVazia = "";
+console.log(variavelVazia);
+
+// Declarando variavel com valor
+let variavelValor = 1;
+console.log(variavelValor);
