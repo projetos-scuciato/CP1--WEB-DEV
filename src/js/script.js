4f2cee8b3b748b6e8f6aa2c6226106653b3b55b4
Pedro Almeida <pedrominato23@gmail.com>
2025-04-18 13:52:38 -0300

    3- Exercicio 2

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,31 @@ console.log(variavelVazia);
 // Declarando variavel com valor
 let variavelValor = 1;
 console.log(variavelValor);
+
+// ----------------------------------
+
+// Exercicio 2!
+
+
+// Operadores logicos
+let valor1 = 10;
+let valor2 = 20;
+
+// Diferente
+if (valor1 != valor2){
+    console.log("Os valores são diferentes!");
+}
+
+// Igual
+if (valor1 == valor2){
+    console.log("Os valores são iguais!")
+}
+
+// Maior ou igual
+if (valor1 >= valor2){
+    console.log(`O valor1 ${valor1} é maior ou igual que o valor2 ${valor2}`)
+} else {
+    console.log(`O valor1 (${valor1}) é menor que o valor2 (${valor2})`)
+}
+
+// ----------------------------------
